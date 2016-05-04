28d0abc99471cf3ada7954c7bb984e39a6a14b54
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-04 19:23:11 +0000

    3-args.c done

--- 3-args.c
+++ 3-args.c
@@ -1,8 +1,15 @@
 #include "my_functions.h"
 
-int main(void)
+int main(int argc, char *argv[])
 {
-  print_char('\n');
+  int i, j;
+
+  for (i = 0; i < argc; i++)	/* i counts the number of strings */
+  {
+    for (j = 0; argv[i][j] != '\0'; j++)
+      print_char(argv[i][j]);	/* print char */
+    print_char('\n');		/* new line */
+  }
 
   return 0;
 }
